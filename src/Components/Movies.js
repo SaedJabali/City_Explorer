54b4dd7154aa49ed8ff3f4d127fdfac4b64563c6
saedjabali <saad.jabali@yahoo.com>
2021-05-08 17:41:30 +0300

    fixed movies

--- src/Components/Movies.js
+++ src/Components/Movies.js
@@ -9,7 +9,7 @@ export class Movie extends Component {
                     <Card className='card' style={{ width: '18rem', fontSize: '24px', }}>
                             <Card.Title style={{ fontSize: '30px', fontWeight: 'bolder' }}>{movie.title}</Card.Title>
                         <Card.Body>
-                        <Card.Img variant="top" src={movie.image} />
+                        <Card.Img width='30%' variant="top" src={`https://image.tmdb.org/t/p/original${movie.image}`} />
                         </Card.Body>
                         <Card.Text>
                             {`Released on  ${movie.released_date}`}
